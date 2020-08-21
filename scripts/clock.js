0017524adc83646c225d9675e960e58485d3dc26
Kaan <kaankucuk2006@gmail.com>
2020-08-22 01:48:37 +0300

    MORE organisation

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,5 +1,10 @@
 function clock() {
     "use strict";
+
+    var timer1 = 0; //start from 0
+    var timer2 = 0; //start from 0
+    var timer3 = 0; //start from 0
+    
     if (timer1 < 60) {
         timer1 += 1;
         document.getElementById("clock").innerHTML = timer3 + ":" + timer2 + ":" + timer1; //puts timer to html
--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,6 @@
 setInterval, document
 */
 
-var timer1 = 0; //start from 0
-var timer2 = 0; //start from 0
-var timer3 = 0; //start from 0
-
 setInterval(clock, 1); //for test purposes
 // setInterval(clock, 1000); 
 // for  normal purposes
