2c84fbec8626daabe7ec63874fb0a27b513be80a
Kaank2006 <57836764+Kaank2006@users.noreply.github.com>
2020-08-21 17:14:39 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+/*global
+setInterval, document
+*/
+
+var timer1 = 0; //start from 0
+var timer2 = 0; //start from 0
+var timer3 = 0; //start from 0
+
+function clock() {
+    "use strict";
+    if (timer1 < 60) {
+        timer1 += 1;
+        document.getElementById("clock").innerHTML = timer3 + ":" + timer2 + ":" + timer1;
+    } else {
+        if (timer1 === 60) {
+            timer1 = 0;
+            timer2 += 1;
+            if (timer2 === 60) {
+                timer2 = 0;
+                timer3 += 1;
+            }
+        }
+    }
+}
+setInterval(clock, 1); //for test purposes
+// setInterval(clock, 1000); 
+// for  normal purposes
