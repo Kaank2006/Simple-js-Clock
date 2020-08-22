3ec48092bf4b5fcd013a79cd30cf2f7eb5298ffa
Kaan Küçük <kaankucuk2006@gmail.com>
2020-08-22 19:57:44 +0300

    Fixing more messy stuff

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,3 +1,6 @@
+/*global
+document
+*/
 var timer1 = 0; //start from 0
 var timer2 = 0; //start from 0
 var timer3 = 0; //start from 0
@@ -7,8 +10,7 @@ function clock() {
     if (timer1 < 60) {
         timer1 += 1;
         document.getElementById("clock").innerHTML = timer3 + ":" + timer2 + ":" + timer1; //puts timer to html
-    }
-    else {
+    } else {
         if (timer1 === 60) { //seconds to minutes
             timer1 = 0;
             timer2 += 1;
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 /*global
-setInterval, document
+setInterval
 */
 
-setInterval(clock, 1); //for test purposes
-// setInterval(clock, 1000); 
-// for  normal purposes
+// setInterval(clock, 1); 
+//for test purposes
+setInterval(clock, 1000); // for  normal purposes
